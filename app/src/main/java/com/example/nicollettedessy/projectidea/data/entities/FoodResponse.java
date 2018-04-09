11516f5e78c9527624644bb171c22cbb76cce58c
Pierre Thelusma <pthelusma@gmail.com>
2018-04-08 22:29:25 -0400

    Adding ignore file so that local files aren't accidentally checked in

---
+++ app/src/main/java/com/example/nicollettedessy/projectidea/data/entities/FoodResponse.java
@@ -0,0 +1,8 @@
+package com.example.nicollettedessy.projectidea.data.entities;
+
+/**
+ * Created by pierrethelusma on 4/8/18.
+ */
+
+public class FoodResponse {
+}
---
+++ app/src/main/java/com/example/nicollettedessy/projectidea/services/FoodItemCollectionProvider.java
@@ -0,0 +1,23 @@
+package com.example.nicollettedessy.projectidea.models;
+
+import com.example.nicollettedessy.projectidea.data.entities.SearchResponseListItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by pierrethelusma on 4/8/18.
+ */
+
+public class FoodItemCollectionProvider {
+
+    public static final ArrayList<SearchResponseListItem> ITEMS = new ArrayList<SearchResponseListItem>();
+
+    public static final Map<String, SearchResponseListItem> ITEM_MAP = new HashMap<String, SearchResponseListItem>();
+
+    public static void addItem(SearchResponseListItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.ndbno, item);
+    }
+}
